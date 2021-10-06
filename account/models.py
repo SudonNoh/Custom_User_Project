712895f5d6596d6e6d99bd91f81a8bf8b58f60c2
sudonnoh <sudonnoh@gmail.com>
2021-10-06 16:45:35 +0900

    models.py/managers.py updated.

--- account/models.py
+++ account/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import UserManager
 
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(_("email address"), unique=True)
+    username =models.CharField(_("user name"), max_length=255)
+    is_active = models.BooleanField(_('is active'), defalt=True)
+    is_staff = models.BooleanField(_('is staff'), default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
