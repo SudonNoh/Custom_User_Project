85975cec46bd74fa2a8a4c346e473d1ccfe3fc76
sudonnoh <sudonnoh@gmail.com>
2021-10-07 16:35:26 +0900

    reset

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import UserManager
+
+# Create your models here.
