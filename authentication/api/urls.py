9e5d981127fbc9bc16297d6269317968b5e9c8c6
sudonnoh <sudonnoh@gmail.com>
2021-10-13 11:09:35 +0900

    loggin a user in with Postman

--- authentication/api/urls.py
+++ authentication/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import RegistrationAPIView
+from .views import (
+    RegistrationAPIView, LoginAPIView
+)
 
 
 urlpatterns = [
     path('registration/', RegistrationAPIView.as_view()),
+    path('login/', LoginAPIView.as_view()),
 ]
 
 
--- authentication/renderers.py
+++ authentication/renderers.py
@@ -2,8 +2,23 @@ import json
 from rest_framework.renderers import JSONRenderer
 
 
+# renderer 에 대해서 더 공부할 필요가 있음
 class UserJSONRenderer(JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, media_type=None, renderer_context=None):
+        # If we recieve a 'token' key as part of the response, it will by a
+        # byte object. Byte objects don't serializer well, so we need to
+        # decode it before rendering the User object.
+        token = data.get('token', None)
         
+        # isinstance(a, bytes) "a"가 bytes 형이면 True, 아니면 False
+        if token is not None and isinstance(token, bytes):
+            # Also as mentioned above, we will decode 'token' if it is type
+            # bytes.
+            data['token'] = token.decode('utf-8')
+            
+        # Finally, we can render our data under the 'user' namespace.
+        return json.dumps({
+            'user':data
+        })
